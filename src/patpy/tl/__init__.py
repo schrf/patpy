8b825faafcbec4b363285daceb326f3a5f450da9
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2026-04-27 16:17:23 +0200

    Fixed broken list

--- src/patpy/tl/__init__.py
+++ src/patpy/tl/__init__.py
@@ -1,37 +1,17 @@
-from .evaluation import (
-    evaluate_prediction,
-    evaluate_representation,
-    persistence_evaluation,
-    predict_knn,
-    test_distances_significance,
-    test_proportions,
-    associate_embedding_with_covariates,
-)
-from .sample_representation import (
-    MOFA,
-    PILOT,
-    CellGroupComposition,
-    DiffusionEarthMoverDistance,
-    GloScope,
-    GloScope_py,
-    GroupedPseudobulk,
-    MrVI,
-    Pseudobulk,
-    RandomVector,
-    SampleRepresentationMethod,
-    SCPoli,
-    WassersteinTSNE,
-    correlate_cell_type_expression,
-    correlate_composition,
-    describe_metadata,
-)
-from .condition_comparison import (
-    ConditionComparison,
-    build_all_pairwise_contrasts,
-    build_condition_combinations,
-    filter_adata_to_conditions,
-from .supervised import (
-    PULSAR,
-    MixMIL,
-    PaSCient,
-)
+from .condition_comparison import (ConditionComparison,
+                                   build_all_pairwise_contrasts,
+                                   build_condition_combinations,
+                                   filter_adata_to_conditions)
+from .evaluation import (associate_embedding_with_covariates,
+                         evaluate_prediction, evaluate_representation,
+                         persistence_evaluation, predict_knn,
+                         test_distances_significance, test_proportions)
+from .sample_representation import (MOFA, PILOT, CellGroupComposition,
+                                    DiffusionEarthMoverDistance, GloScope,
+                                    GloScope_py, GroupedPseudobulk, MrVI,
+                                    Pseudobulk, RandomVector,
+                                    SampleRepresentationMethod, SCPoli,
+                                    WassersteinTSNE,
+                                    correlate_cell_type_expression,
+                                    correlate_composition, describe_metadata)
+from .supervised import PULSAR, MixMIL, PaSCient
